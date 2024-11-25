bbe6c2ac546fa20727cbf6632a4628e25d688b68
aliawan-dot <243638@students.au.edu.pk>
2024-11-25 22:07:45 +0500

    Update program.cpp

--- program.cpp
+++ program.cpp
@@ -8,15 +8,15 @@ int main() {
     cout << "Enter three numbers: ";
     cin >> n1 >> n2 >> n3;
 
-   
+   // if n1 is the biggest,then display it
     if(n1 >= n2 && n1 >= n3)
         cout << "Largest number: " << n1;
 
-    
+    //if n2 is the biggest,then display it
     else if(n2 >= n1 && n2 >= n3)
         cout << "Largest number: " << n2;
     
-   
+   //if n1 and n2 is not,then n3 is the biggest
     else 
         cout << "Largest number: " << n3;
   
